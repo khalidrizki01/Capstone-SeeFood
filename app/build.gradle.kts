f8d19e8de637cba69e27730831b05cc8dd7b4f48
Haehikan <hfzhprst@gmail.com>
2023-11-04 16:57:30 +0700

    Added Barchart and Piechart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
         viewBinding = true
     }
+
 
     buildTypes {
         release {
@@ -51,7 +51,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation ("com.diogobernardino:williamchart:3.10.1")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
