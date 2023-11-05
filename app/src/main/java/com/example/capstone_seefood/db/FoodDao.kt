374a9f43a3d98c78f94bade677b28bb2e09c8054
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2023-11-05 19:35:31 +0700

    Fix FoodDao

--- app/src/main/java/com/example/capstone_seefood/db/FoodDao.kt
+++ app/src/main/java/com/example/capstone_seefood/db/FoodDao.kt
@@ -1,10 +1,12 @@
 package com.example.capstone_seefood.db
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
+@Dao
 interface FoodDao {
     @Query("SELECT * FROM food")
     fun getAllFoods():List<Food>
