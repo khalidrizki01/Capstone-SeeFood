9caf91b0f884ae1fb121f29f46a87d758893aff3
Haehikan <hfzhprst@gmail.com>
2023-11-08 23:16:40 +0700

    add predict activity(untested)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,13 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
     implementation("org.pytorch:pytorch_android_lite:1.10.0")
     implementation("org.pytorch:pytorch_android_torchvision:1.10.0")
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-video:1.3.0")
+    implementation("androidx.camera:camera-extensions:1.3.0")
+
 
     val room_version = "2.6.0"
 
--- app/src/main/java/com/example/capstone_seefood/PredictActivity.java
+++ app/src/main/java/com/example/capstone_seefood/PredictActivity.java
@@ -1,11 +1,16 @@
 package com.example.capstone_seefood;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageProxy;
 
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
@@ -19,23 +24,74 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class PredictActivity  extends AppCompatActivity {
-    List<String> imagenet_classes;
+    private Bitmap bitmap = null;
 
+    List<String> imagenet_classes;
+    Module module;
+
+    public static String assetFilePath(Context context, String assetName)throws IOException{
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try(InputStream is = context.getAssets().open(assetName)){
+            try(OutputStream os = new FileOutputStream(file)){
+                byte[] buffer = new byte[4*1024];
+                int read;
+                while ((read=is.read(buffer))!=-1){
+                    os.write(buffer,0,read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predict);
-        imagenet_classes = LoadClasses("")
+        try {
+            bitmap = BitmapFactory.decodeStream(getAssets().open("PATH SAVED FOTO"));
+            module = module.load(assetFilePath(this, "best.pt"));
+        }
+        catch(IOException e) {
+            Log.e("PTRT", "error no module");
+            finish();
+        }
+        var previewView = findViewById(R.id.previewscan);
+
+        imagenet_classes = LoadClasses("");
         LoadTorchModule("best.pt");
 
+
+
+        final Button button = findViewById(R.id.btnprediksi);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                @SuppressLint("UnsafeOptInUsageError") Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap, TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+                Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+                float[] scores = outputTensor.getDataAsFloatArray();
+                float maxScore = -Float.MAX_VALUE;
+                int maxScoreIdx = -1;
+                for (int i = 0; i < scores.length; i++) {
+                    if (scores[i] > maxScore) {
+                        maxScore = scores[i];
+                        maxScoreIdx = i;
+                    }
+                }
+
+            }
+        });
     }
 
-    Module module;
+
 
     void LoadTorchModule(String fileName) {
         File modelFile = new File(this.getFilesDir(), fileName);
@@ -59,24 +115,11 @@ public class PredictActivity  extends AppCompatActivity {
 
     }
 
-    void analyzeImage(ImageProxy image, int rotation) {
-        @SuppressLint("UnsafeOptInUsageError") Tensor inputTensor = TensorImageUtils.imageYUV420CenterCropToFloat32Tensor(image.getImage(), rotation, 244, 244,
-                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-        float[] scores = outputTensor.getDataAsFloatArray();
-        float maxScore = -Float.MAX_VALUE;
-        int maxScoreIdx = -1;
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] > maxScore) {
-                maxScore = scores[i];
-                maxScoreIdx = i;
-            }
-
-        }
 
 
 
-        }
+
+
     List<String> LoadClasses (String fileName){
         List<String> classes = new ArrayList<>();
         try{
