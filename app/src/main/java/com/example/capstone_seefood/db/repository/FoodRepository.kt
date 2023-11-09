db10477984c66675594c7fb4440bb78d58d7f90d
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2023-11-09 11:54:03 +0700

    Add template for repository

---
+++ app/src/main/java/com/example/capstone_seefood/db/repository/FoodRepository.kt
@@ -0,0 +1,12 @@
+package com.example.capstone_seefood.db.repository
+
+//import com.example.capstone_seefood.db.FoodDao
+//
+//class FoodRepository (private val foodDao : FoodDao) {
+//
+//
+//    fun insert(){
+//        foodDao.
+//    }
+//
+//}
---
+++ app/src/main/java/com/example/capstone_seefood/ui/MainViewModelFactory.kt
@@ -0,0 +1,11 @@
+package com.example.capstone_seefood.ui
+//
+//import com.example.capstone_seefood.di.Injection
+//
+//class MainViewModelFactory  {
+//
+//    val z = Injection.provideDao()
+//
+//    return MainViewModel()
+
+//}
