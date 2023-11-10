d45ebf4fec29115377e11b8ffd80cc61fc0fcf51
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2023-11-11 06:40:45 +0700

    Create Start Camera Activity

--- app/src/main/java/com/example/capstone_seefood/RealtimeActivity.kt
+++ app/src/main/java/com/example/capstone_seefood/RealtimeActivity.kt
@@ -1,8 +1,10 @@
 package com.example.capstone_seefood
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.*
+import com.example.capstone_seefood.ml.BestFloat32
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
@@ -14,7 +16,7 @@ import android.view.Surface
 import android.view.TextureView
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
-import com.example.realtime_object.ml.SsdMobilenetV11Metadata1
+//import com.example.realtime_object.ml.SsdMobilenetV11Metadata1
 import org.tensorflow.lite.support.common.FileUtil
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
@@ -34,16 +36,16 @@ class RealtimeActivity : AppCompatActivity() {
     lateinit var handler: Handler
     lateinit var cameraManager: CameraManager
     lateinit var textureView: TextureView
-    lateinit var model:"inputmodel"
+//    lateinit var model:"inputmodel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_Realtime)
+        setContentView(R.layout.activity_realtime)
         get_permission()
 
         labels = FileUtil.loadLabels(this, "labels.txt")
         imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
-        model = "inputmodel".newInstance(this)
+//        model = "inputmodel".newInstance(this)
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
@@ -67,33 +69,33 @@ class RealtimeActivity : AppCompatActivity() {
                 var image = TensorImage.fromBitmap(bitmap)
                 image = imageProcessor.process(image)
 
-                val outputs = model.process(image)
-                val locations = outputs.locationsAsTensorBuffer.floatArray
-                val classes = outputs.classesAsTensorBuffer.floatArray
-                val scores = outputs.scoresAsTensorBuffer.floatArray
-                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
-
-                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-                val canvas = Canvas(mutable)
-
-                val h = mutable.height
-                val w = mutable.width
-                paint.textSize = h/15f
-                paint.strokeWidth = h/85f
-                var x = 0
-                scores.forEachIndexed { index, fl ->
-                    x = index
-                    x *= 4
-                    if(fl > 0.5){
-                        paint.setColor(colors.get(index))
-                        paint.style = Paint.Style.STROKE
-                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
-                        paint.style = Paint.Style.FILL
-                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h, paint)
-                    }
-                }
-
-                imageView.setImageBitmap(mutable)
+//                val outputs = BestFloat32.process(image)
+//                val locations = outputs.locationsAsTensorBuffer.floatArray
+//                val classes = outputs.classesAsTensorBuffer.floatArray
+//                val scores = outputs.scoresAsTensorBuffer.floatArray
+//                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
+//
+//                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+//                val canvas = Canvas(mutable)
+//
+//                val h = mutable.height
+//                val w = mutable.width
+//                paint.textSize = h/15f
+//                paint.strokeWidth = h/85f
+//                var x = 0
+//                scores.forEachIndexed { index, fl ->
+//                    x = index
+//                    x *= 4
+//                    if(fl > 0.5){
+//                        paint.setColor(colors.get(index))
+//                        paint.style = Paint.Style.STROKE
+//                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
+//                        paint.style = Paint.Style.FILL
+//                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h, paint)
+//                    }
+//                }
+//
+//                imageView.setImageBitmap(mutable)
 
 
             }
@@ -105,7 +107,7 @@ class RealtimeActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        model.close()
+//        model.close()
     }
 
     @SuppressLint("MissingPermission")
