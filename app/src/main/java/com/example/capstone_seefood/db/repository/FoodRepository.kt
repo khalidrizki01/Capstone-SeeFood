a9d06c670a02b4eabb9cf8d96b38378e852a65ec
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2023-11-16 12:32:15 +0700

    Add template for View Model

--- app/src/main/java/com/example/capstone_seefood/db/repository/FoodRepository.kt
+++ app/src/main/java/com/example/capstone_seefood/db/repository/FoodRepository.kt
@@ -1,46 +1,45 @@
 package com.example.capstone_seefood.db.repository
 
-import com.example.capstone_seefood.db.Food
-import com.example.capstone_seefood.db.FoodDao
-import com.example.capstone_seefood.db.FoodDatabase
-import com.example.capstone_seefood.db.Receipt
-import com.example.capstone_seefood.db.relations.ReceiptFoodCrossRef
-import java.time.LocalDateTime
-
+//import com.example.capstone_seefood.db.Food
+//import com.example.capstone_seefood.db.FoodDao
+//import com.example.capstone_seefood.db.FoodDatabase
+//import com.example.capstone_seefood.db.Receipt
+//import com.example.capstone_seefood.db.relations.ReceiptFoodCrossRef
+//import java.time.LocalDateTime
+//import java.util.UUID
 //
-class FoodRepository (private val foodDatabase : FoodDatabase) {
-    suspend fun insertFood(food: Food){
-        foodDatabase.foodDao.insertFood(food)
-    }
-    suspend fun insertReceipt(receipt: Receipt) {
-        foodDatabase.foodDao.insertReceipt(receipt)
-    }
-    suspend fun insertReceiptFoodCrossRef(crossRef: ReceiptFoodCrossRef) {
-        foodDatabase.foodDao.insertReceiptFoodCrossRef(crossRef)
-    }
-    fun getAllReceiptFoodCrossRef() = foodDatabase.foodDao.getAllReceiptFoodCrossRef()
-    fun getAvailableFood() = foodDatabase.foodDao.getAvailableFood()
-    fun geUnavailableFood() =  foodDatabase.foodDao.getUnavailableFood()
-    fun getTotalItemPricePerFoodId(startDate: LocalDateTime){
-        foodDatabase.foodDao.getTotalItemPricePerFoodId(startDate)
-    }
-    fun getQuantityPerFoodId(startDate: LocalDateTime) {
-        foodDatabase.foodDao.getQuantityPerFoodId(startDate)
-    }
-    suspend fun getFoodBasedOnName(foodName : String) {
-        foodDatabase.foodDao.getFoodBasedOnName(foodName)
-    }
-    fun getReceiptFrom(date : LocalDateTime) = foodDatabase.foodDao.getReceiptFrom(date)
-    suspend fun deleteAllFood() {
-        foodDatabase.foodDao.deleteAllFood()
-    }
-    suspend fun deleteAllReceipts() {
-        foodDatabase.foodDao.deleteAllReceipts()
-    }
-    suspend fun deleteAllReceiptFoodCR(){
-        foodDatabase.foodDao.deleteAllReceiptFoodCR()
-    }
-    suspend fun UpdateFood(food: Food){
-        foodDatabase.foodDao.updateFood(food)
-    }
-}
+////
+//class FoodRepository (private val foodDatabase : FoodDatabase) {
+//    suspend fun insertFood(food: Food){
+//        foodDatabase.foodDao.insertFood(food)
+//    }
+//    suspend fun insertReceipt(receipt: Receipt) {
+//        foodDatabase.foodDao.insertReceipt(receipt)
+//    }
+//    suspend fun insertReceiptFoodCrossRef(crossRef: ReceiptFoodCrossRef) {
+//        foodDatabase.foodDao.insertReceiptFoodCrossRef(crossRef)
+//    }
+//    fun getReceiptFoodCrossRefByReceiptId(recId: UUID) = foodDatabase.foodDao.getReceiptFoodCrossRefByReceiptId(recId)
+//    fun getAllReceiptFoodCrossRef() = foodDatabase.foodDao.getAllReceiptFoodCrossRef()
+//    fun getAvailableFood() = foodDatabase.foodDao.getAvailableFood()
+//    fun getUnavailableFood() =  foodDatabase.foodDao.getUnavailableFood()
+//    fun getTotalItemPricePerFoodId(startDate: LocalDateTime) = foodDatabase.foodDao.getTotalItemPricePerFoodId(startDate)
+//
+//    fun getQuantityPerFoodId(startDate: LocalDateTime) = foodDatabase.foodDao.getQuantityPerFoodId(startDate)
+//
+//    fun getFoodBasedOnName(foodName : String) = foodDatabase.foodDao.getFoodBasedOnName(foodName)
+//
+//    fun getReceiptFrom(date : LocalDateTime) = foodDatabase.foodDao.getReceiptFrom(date)
+//    suspend fun deleteAllFood() {
+//        foodDatabase.foodDao.deleteAllFood()
+//    }
+//    suspend fun deleteAllReceipts() {
+//        foodDatabase.foodDao.deleteAllReceipts()
+//    }
+//    suspend fun deleteAllReceiptFoodCR(){
+//        foodDatabase.foodDao.deleteAllReceiptFoodCR()
+//    }
+//    suspend fun updateFood(food: Food){
+//        foodDatabase.foodDao.updateFood(food)
+//    }
+//}
--- app/src/main/java/com/example/capstone_seefood/ui/FoodViewModel.kt
+++ app/src/main/java/com/example/capstone_seefood/ui/FoodViewModel.kt
@@ -1,22 +1,62 @@
 package com.example.capstone_seefood.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
-import com.example.capstone_seefood.db.Food
-import com.example.capstone_seefood.db.Receipt
-import com.example.capstone_seefood.db.relations.ReceiptFoodCrossRef
-import com.example.capstone_seefood.db.repository.FoodRepository
-import kotlinx.coroutines.launch
-import java.time.LocalDateTime
-
-class MainViewModel (private val foodRepository: FoodRepository) : ViewModel() {
-    fun insertFood(foodName : String, photo : String, price : Int, isSell){
-        viewModelScope.launch {
-            foodRepository.insertFood(Food(name = foodName, photo = photo, price = price, isSell = isSell))
-        }
-    }
-
-
-}
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.asLiveData
+//import androidx.lifecycle.viewModelScope
+//import com.example.capstone_seefood.db.Food
+//import com.example.capstone_seefood.db.Receipt
+//import com.example.capstone_seefood.db.relations.ReceiptFoodCrossRef
+//import com.example.capstone_seefood.db.repository.FoodRepository
+//import kotlinx.coroutines.launch
+//import java.time.LocalDateTime
+//import java.util.Date
+//import java.util.UUID
+//
+//class FoodViewModel (private val foodRepository: FoodRepository) : ViewModel() {
+//    fun insertFood(foodName : String, photo : String, price : Int, isSell : Boolean = true){
+//        viewModelScope.launch {
+//            foodRepository.insertFood(Food(name = foodName, photo = photo, price = price, isSell = isSell))
+//        }
+//    }
+//
+//    fun insertReceipt(receiptId : UUID, totalPrice : Int){
+//        viewModelScope.launch {
+//            foodRepository.insertReceipt(Receipt(receiptId = receiptId, totalPrice = totalPrice))
+//        }
+//    }
+//    fun insertReceiptFoodCrossRef(receiptId: UUID, foodId : Int, foodName: String, foodPrice : Int, quantity : Int) {
+//        viewModelScope.launch {
+//            foodRepository.insertReceiptFoodCrossRef(ReceiptFoodCrossRef(receiptId = receiptId, foodId = foodId, foodName = foodName, foodPrice = foodPrice, quantity = quantity))
+//        }
+//    }
+//    fun getReceiptFrom(date: LocalDateTime) = foodRepository.getReceiptFrom(date).asLiveData(viewModelScope.coroutineContext)
+//
+//    fun getReceiptFoodCrossRefByReceiptId(recId: UUID) = foodRepository.getReceiptFoodCrossRefByReceiptId(recId).asLiveData(viewModelScope.coroutineContext)
+//    fun getAllReceiptFoodCrossRef() = foodRepository.getAllReceiptFoodCrossRef().asLiveData(viewModelScope.coroutineContext)
+//    fun getAvailableFood() = foodRepository.getAvailableFood().asLiveData(viewModelScope.coroutineContext)
+//    fun getUnavailableFood() = foodRepository.getUnavailableFood().asLiveData(viewModelScope.coroutineContext)
+//    fun getTotalItemPricePerFoodId(startDate : LocalDateTime) = foodRepository.getTotalItemPricePerFoodId(startDate).asLiveData(viewModelScope.coroutineContext)
+//    fun getQuantityPerFoodId(startDate: LocalDateTime) = foodRepository.getQuantityPerFoodId(startDate).asLiveData(viewModelScope.coroutineContext)
+//    fun getFoodBasedOnName(foodName : String) = foodRepository.getFoodBasedOnName(foodName).asLiveData(viewModelScope.coroutineContext)
+//
+//    fun deleteAllFood(){
+//        viewModelScope.launch {
+//            foodRepository.deleteAllFood()
+//        }
+//    }
+//    fun deleteAllReceipts(){
+//        viewModelScope.launch {
+//            foodRepository.deleteAllReceipts()
+//        }
+//    }
+//    fun deleteAllReceiptFoodCR() {
+//        viewModelScope.launch {
+//            foodRepository.deleteAllReceiptFoodCR()
+//        }
+//    }
+//    fun updateFood(food: Food) {
+//        viewModelScope.launch {
+//            foodRepository.updateFood(food)
+//        }
+//    }
+//}
